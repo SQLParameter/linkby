15e9f1556ace7bc13865cb20903a535ee7009513
zhangdezheng@utry.cn <zhangdezheng@utry.cn>
2018-04-09 17:26:37 +0800

    添加滑动删除
    Signed-off-by: zhangdezheng@utry.cn <zhangdezheng@utry.cn>

--- pages/teacher/class/manage/manage.js
+++ pages/teacher/class/manage/manage.js
@@ -1,13 +1,71 @@
 Page({
   data: {
-    list: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+    list: [{
+      isTouchMove: false
+    }, {
+      isTouchMove: false
+    }, {
+      isTouchMove: false
+    }],
+    startX: 0, //开始坐标
+    startY: 0, //开始坐标
   },
   onLoad: function () {
 
   },
-  tGrade:function(){
+  tGrade: function () {
     wx.redirectTo({
       url: '/pages/teacher/create/grade/grade'
     })
+  },
+  touchstart: function (e) {
+    //开始触摸时 重置所有删除
+    this.data.list.forEach(function (v, i) {
+      if (v.isTouchMove)//只操作为true的
+        v.isTouchMove = false;
+    })
+    this.setData({
+      startX: e.changedTouches[0].clientX,
+      startY: e.changedTouches[0].clientY,
+      list: this.data.list
+    })
+  },
+  //滑动事件处理
+  touchmove: function (e) {
+
+    var that = this,
+      index = e.currentTarget.dataset.index,//当前索引
+      startX = that.data.startX,//开始X坐标
+      startY = that.data.startY,//开始Y坐标
+      touchMoveX = e.changedTouches[0].clientX,//滑动变化坐标
+      touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
+      //获取滑动角度
+      angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
+    that.data.list.forEach(function (v, i) {
+      v.isTouchMove = false
+      //滑动超过30度角 return
+      if (Math.abs(angle) > 30) return;
+      if (i == index) {
+        if (touchMoveX > startX) //右滑
+          v.isTouchMove = false
+        else //左滑
+          v.isTouchMove = true
+      }
+    })
+    //更新数据
+    that.setData({
+      list: that.data.list
+    })
+  },
+  /**
+   * 计算滑动角度
+   * @param {Object} start 起点坐标
+   * @param {Object} end 终点坐标
+   */
+  angle: function (start, end) {
+    var _X = end.X - start.X,
+      _Y = end.Y - start.Y
+    //返回角度 /Math.atan()返回数字的反正切值
+    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
   }
 })
