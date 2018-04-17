2123754f271deb973f091566600f2a8e06a6e351
ospanpan0 <289774716@qq.com>
2018-04-17 20:58:59 +0800

    教师班级圈发布动态

--- pages/teacher/circle/createCircle/createCircle.js
+++ pages/teacher/circle/createCircle/createCircle.js
@@ -1,16 +1,39 @@
 Page({
   data: {
+    content: '',
+    imageIds: [],
     files: []
   },
   chooseImage: function (e) {
     var that = this;
+    var app = getApp();
     wx.chooseImage({
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        that.setData({
-          files: that.data.files.concat(res.tempFilePaths)
+        var tempFilePaths = res.tempFilePaths
+        wx.uploadFile({
+          url: app.globalData.api_URL.UploadUrl, 
+          filePath: tempFilePaths[0],
+          name: "file",
+          formData: {
+            'classesId': app.globalData.userInfo.teacher.curClassesId,
+            'schoolId': app.globalData.userInfo.teacher.schoolId,
+            'objType': 1
+           },
+          success: function (res) {
+            console.log("res=" + JSON.stringify(res));
+            if (res.statusCode == 200) {
+              var data = JSON.parse(res.data);
+              if (data.data != null && data.data.id.length > 0) {
+                that.data.imageIds.push(data.data.id);
+                // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+                that.setData({
+                  files: that.data.files.concat(data.data.baseUrl + '/' + data.data.relativeUrl)
+                });
+              }
+            }
+          }
         });
       }
     })
@@ -29,5 +52,47 @@ Page({
   },
   deleteEnter: function () {
     this.setData({ hideview: false });
-  }
+  },
+  //监听内容输入
+  content_input: function(event){
+    if (event.detail.value.trim().length > 200) {
+      this.setData({ content: event.detail.value.trim().substring(0, 200) });
+    } else {
+      this.setData({ content: event.detail.value.trim() });
+    }
+  },
+  //动态发布保存
+  pubDynamics: function(){
+    if (this.data.content.length == 0 && imageIds.length==0){
+      wx.showToast({ title: '请输入内容或选择图片' });
+      return;
+    }
+    var app = getApp();
+    var curModule = this;
+    app.post_api_data(app.globalData.api_URL.PubDynamics,
+      {
+        'classesId': app.globalData.userInfo.teacher.curClassesId,
+        'schoolId': app.globalData.userInfo.teacher.schoolId,
+        'content': curModule.data.content,
+        'imageIds': curModule.data.imageIds.join(","),
+        'type': 1,
+        'alias': app.globalData.userInfo.teacher.realName + "老师",
+        'userId': app.globalData.userInfo.user.id
+      },
+      function (data) {
+        if (data.apiStatus == "200") {
+          wx.showToast({ title: '发布成功' });
+          var pages = getCurrentPages();
+          var prevPage = pages[pages.length - 2];
+          prevPage.getDynamicsList();
+          wx.navigateBack();
+        } else {
+          wx.showToast({ title: data.msg });
+        }
+      }, function (err) {
+        wx.showToast({ title: '操作失败' });
+      });
+
+  },
+
 })
