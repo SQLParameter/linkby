c3b58e0b3e024677da2a44386fc41c25351446b3
ospanpan0 <289774716@qq.com>
2018-04-17 21:06:16 +0800

    作业查看

--- pages/teacher/task/check/check.js
+++ pages/teacher/task/check/check.js
@@ -3,17 +3,27 @@ Page({
     wwc:false,
     isShowDetails:false,
     detailsHeight:'80rpx;',
-    itemList:[
-      {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
-    ]
+    homeworkId: '',
+    homeWork: { content: '', imageIds: '', totalStudents: 0, finishedStudents: 0 },
+    unFinishedList: [],
+    finishedList: []
   },
-  onLoad: function () {
-
+  onLoad: function (option) {
+    var curModule = this;
+    curModule.setData({ homeworkId: option.homeworkId });
+    curModule.getHomeworkDetails(function () {
+      curModule.getHomeworkCompletion();
+    });
   },
-  tGrade:function(){
-    wx.redirectTo({
-      url: '/pages/teacher/create/grade/grade'
-    })
+  imgYu: function (event) {
+    var src = event.currentTarget.dataset.src;//获取data-src
+    if (src !='/icons/id_000001_img.png'){
+      //图片预览  本地图片不能预览
+      wx.previewImage({
+        current: src, // 当前显示图片的http链接
+        urls: [src] // 需要预览的图片http链接列表
+      });
+    }    
   },
   noComplete:function(){
     this.setData(
@@ -35,7 +45,52 @@ Page({
       this.setData(
         { isShowDetails: false, detailsHeight: '80rpx' }
       );
-    }
-    
+    }    
+  },
+
+  //获取作业详细信息
+  getHomeworkDetails: function (sucFun) {
+    var curModule = this;
+    var app = getApp();
+    app.get_api_data(app.globalData.api_URL.GetHomeworkDetails,
+      {
+        'classesId': app.globalData.userInfo.teacher.curClassesId,
+        'schoolId': app.globalData.userInfo.teacher.schoolId,
+        'homeworkId': curModule.data.homeworkId
+      },
+      function (data) {
+        if (data.apiStatus == "200") {
+          curModule.setData({ homeWork: data.data });
+          if (typeof (sucFun) == "function") {
+            sucFun(data);
+          }
+        } else {
+          wx.showToast({ title: data.msg });
+        }
+      }, function () {
+        wx.showToast({ title: "获取失败" });
+      });
+  },
+  //获取作业完成情况
+  getHomeworkCompletion: function () {
+    var curModule = this;
+    var app = getApp();
+    app.get_api_data(app.globalData.api_URL.GetHomeworkCompletion,
+      {
+        'classesId': app.globalData.userInfo.teacher.curClassesId,
+        'schoolId': app.globalData.userInfo.teacher.schoolId,
+        'homeworkId': curModule.data.homeworkId
+      },
+      function (data) {
+        console.log(JSON.stringify(data.data));
+        if (data.apiStatus == "200") {
+          curModule.setData({ finishedList: data.data.completeList });
+          curModule.setData({ unFinishedList: data.data.unCompleteList });
+        } else {
+          wx.showToast({ title: data.msg });
+        }
+      }, function () {
+        wx.showToast({ title: "获取失败" });
+      });
   }
 })
--- pages/teacher/task/create/create.js
+++ pages/teacher/task/create/create.js
@@ -8,9 +8,29 @@ Page({
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        that.setData({
-          files: that.data.files.concat(res.tempFilePaths)
+        var tempFilePaths = res.tempFilePaths
+        wx.uploadFile({
+          url: app.globalData.api_URL.UploadUrl,
+          filePath: tempFilePaths[0],
+          name: "file",
+          formData: {
+            'classesId': app.globalData.userInfo.teacher.curClassesId,
+            'schoolId': app.globalData.userInfo.teacher.schoolId,
+            'objType': 0
+          },
+          success: function (res) {
+            console.log("res=" + JSON.stringify(res));
+            if (res.statusCode == 200) {
+              var data = JSON.parse(res.data);
+              if (data.data != null && data.data.id.length > 0) {
+                that.data.imageIds.push(data.data.id);
+                // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+                that.setData({
+                  files: that.data.files.concat(data.data.baseUrl + '/' + data.data.relativeUrl)
+                });
+              }
+            }
+          }
         });
       }
     })
