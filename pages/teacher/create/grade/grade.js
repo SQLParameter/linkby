60f606d8b0cd8c317708d951a171d7e9fd76e6a2
ospanpan0 <289774716@qq.com>
2018-04-17 21:04:41 +0800

    班级创建和管理

--- pages/teacher/create/grade/grade.js
+++ pages/teacher/create/grade/grade.js
@@ -1,13 +1,37 @@
 Page({
   data: {
-    
+    selGradeId: '',
+    selGradeName: '',
+    allGrades: []
   },
-  onLoad: function () {
-
+  onLoad: function (option) {
+    var curModule = this;
+    var app = getApp();
+    app.get_api_data(app.globalData.api_URL.GetAllGrades, {},
+      function (data) {
+        if (data.apiStatus == "200") {
+          curModule.setData({ allGrades: data.data });
+          if (typeof (option.selGradeId) == "string") {
+            curModule.setData({ selGradeId: option.selGradeId });
+          }
+        } else {
+          wx.showToast({ title: data.msg });
+        }
+      }, function () {
+        wx.showToast({ title: "获取失败" });
+      });
   },
-  toInport:function(){
-    wx.redirectTo({
-      url: '/pages/teacher/create/import/import'
-    })
+  onUnload: function () {
+    var pages = getCurrentPages();
+    var prevPage = pages[pages.length - 2];
+    prevPage.setData({
+      gradeId: this.data.selGradeId,
+      gradeName: this.data.selGradeName
+    });
+  },
+  //点击选中年级
+  selectGrade: function (event) {
+    this.setData({ selGradeId: event.currentTarget.dataset.id });
+    this.setData({ selGradeName: event.currentTarget.dataset.name });
   }
 })
