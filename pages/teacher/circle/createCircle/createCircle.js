4a990fe72c40d9f578b51d3b144b5d375bd592f4
ospanpan0 <289774716@qq.com>
2018-04-20 23:12:47 +0800

    添加发布班级圈时一次选多张图片上传

--- pages/teacher/circle/createCircle/createCircle.js
+++ pages/teacher/circle/createCircle/createCircle.js
@@ -5,39 +5,50 @@ Page({
     files: []
   },
   chooseImage: function (e) {
-    var that = this;
+    var curModule = this;
     var app = getApp();
     wx.chooseImage({
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
-        var tempFilePaths = res.tempFilePaths
-        wx.uploadFile({
-          url: app.globalData.api_URL.UploadUrl, 
-          filePath: tempFilePaths[0],
-          name: "file",
-          formData: {
-            'classesId': app.globalData.userInfo.teacher.curClassesId,
-            'schoolId': app.globalData.userInfo.teacher.schoolId,
-            'objType': 1
-           },
-          success: function (res) {
-            console.log("res=" + JSON.stringify(res));
-            if (res.statusCode == 200) {
-              var data = JSON.parse(res.data);
-              if (data.data != null && data.data.id.length > 0) {
-                that.data.imageIds.push(data.data.id);
-                // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-                that.setData({
-                  files: that.data.files.concat(data.data.baseUrl + '/' + data.data.relativeUrl)
-                });
-              }
-            }
-          }
-        });
+        var tempFilePaths = res.tempFilePaths;
+        var index = 0;
+        curModule.uploadImage(tempFilePaths, index, function () { });
       }
     })
   },
+  uploadImage: function (filePaths, index, sucFun) {
+    var app = getApp();
+    var curModule = this;
+    wx.uploadFile({
+      url: app.globalData.api_URL.UploadUrl,
+      filePath: filePaths[index],
+      name: "file",
+      formData: {
+        'classesId': app.globalData.userInfo.teacher.curClassesId,
+        'schoolId': app.globalData.userInfo.teacher.schoolId,
+        'objType': 1
+      },
+      success: function (res) {
+        if (res.statusCode == 200) {
+          var data = JSON.parse(res.data);
+          if (data.data != null && data.data.id.length > 0) {
+            curModule.data.imageIds.push(data.data.id);
+            curModule.setData({
+              files: curModule.data.files.concat(data.data.baseUrl + '/' + data.data.relativeUrl)
+            });
+            if ((index >= filePaths.length - 1) && typeof (sucFun) == "function") {
+              sucFun();
+            } else {
+              index++;
+              curModule.uploadImage(filePaths, index, sucFun);
+            }
+          }
+        }
+      }
+    });
+  },
+
   previewImage: function (e) {
     wx.previewImage({
       current: e.currentTarget.id, // 当前显示图片的http链接
